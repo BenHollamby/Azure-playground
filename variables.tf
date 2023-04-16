ab15d3480530406949218f034518275dc308bad1
Ben Hollamby <ben.hollamby@outlook.com>
2023-04-16 15:37:13 +1200

    linux vm for toying with

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "RG_Location" {
+  type        = string
+  default     = "australiasoutheast"
+  description = "Location of the resource group."
+}
+
+variable "RG_Name" {
+  type        = string
+  default     = "RG_Playground"
+  description = "Name of the resource group."
+}
